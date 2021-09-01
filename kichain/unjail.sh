3515f8714fa408608d755b9891068c45e49e7ca8
Confuzer <CEOpm@protonmail.com>
2021-09-02 02:07:52 +0300

    Upgrading environment(kicli --> kid) and network  upgrade to kichain-t-3. Adding comments.

--- kichain/unjail.sh
+++ kichain/unjail.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 WALLET_PASS="YOUR_PASSWORD_HERE"
+NETWORK=kichain-t-3
+HOME_DIR="YOUR KID HOME PATH"
+kichain_wallet_name="YOUR KICHAIN WALLET"
+
+
 echo 'Listening:'
 while true; do
 current_date=$(date)
-POWER=$(kicli status | jq -r '.[] | .voting_power' | tail -1 | sed -e 's/^"//' -e 's/"$//')
+POWER=$(kid status 2>&1 | jq '.ValidatorInfo.VotingPower | tonumber') # getting our voting power and convert str to int
 
 echo $current_date  'WE HAVE POWER' $POWER
 
-if [ $POWER = 0 ]; then
-    NOW=$(date)
-    echo $NOW 'UNJAILING' 
-    echo -e "$WALLET_PASS\n$WALLET_PASS\n" | kicli tx slashing unjail --chain-id=kichain-t-3 --home $HOME/kichain/kid --from=$kichain_wallet_name --gas=auto --yes
+if [ $POWER = 0 ]; then # if power == 0 then we jailed
+    current_date=$(date) #overriding datetime
+    echo $current_date 'UNJAILING' 
+    echo -e "$WALLET_PASS\n" | kid tx slashing unjail --chain-id=$NETWORK --from=$kichain_wallet_name --gas=auto --yes --home $HOME_DIR
 
 fi
-#echo 'NEXT CHECK IN 15 SEC'
-sleep 15
+sleep 15 # repeating every 15 sec
 done
 
