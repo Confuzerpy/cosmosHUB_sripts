082e3ae9287ff9d4e5aee16197a37f2a7d40799e
Confuzerpy <44144781+Confuzerpy@users.noreply.github.com>
2021-09-13 15:02:43 +0300

    Create kichain_ibc.py
    Python script for making multiple ibc transactions between chains

---
+++ kichain_ibc.py
@@ -0,0 +1,30 @@
+from subprocess import Popen, PIPE
+from random import choice, randint
+from time import sleep
+
+COUNT = 0
+PATH = 'transfer'             # replace with your path
+IBC0 = 'kichain-t-4'          # source chain
+IBC1 = 'testnet-croeseid-4'   # destination chain
+COMMAND = (f"rly tx transfer {IBC0} {IBC1} amountutki "
+           f"$(rly chains address {IBC1}) --path {PATH}",
+           f"rly tx transfer {IBC1} {IBC0} amountbasetcro "
+           f"$(rly chains address {IBC0}) --path {PATH}")
+
+
+def main():
+    """Randomly generate transctions and send them through ibc
+    using Popen module"""
+    while True:
+        try:
+            AMOUNT = randint(1000, 9999) # amount to send randomly generates
+            path = choice(COMMAND).replace('amount', f"{AMOUNT}")
+            COUNT += 1
+            result = Popen(path, stdout=PIPE, shell=True).communicate()[0]
+            print(f"{COUNT} {result}")
+            sleep(60)
+        except Exception as err:
+            print(err.message)
+
+if __name__ == '__main__':
+        main()
