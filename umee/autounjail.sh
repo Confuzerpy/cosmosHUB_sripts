0b1a3f240039594d00cc8f8d4230064de0f14acd
Confuzerpy <CEOpm@protonmail.com>
2021-09-28 03:18:01 +0300

    autounjail script. umee testnet

---
+++ umee/autounjail.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+WALLET_PASS="password"
+NETWORK="umee-betanet-2"
+#HOME_DIR="YOUR umeed HOME PATH"
+wallet="umee1...."
+
+
+echo 'Listening:'
+while true; do
+current_date=$(date)
+POWER=$(umeed status 2>&1 | jq '.ValidatorInfo.VotingPower | tonumber') # getting our voting power and convert str to int
+
+echo $current_date  'Voting power -> ' $POWER
+
+if [ $POWER = 0 ]; then # if power == 0 then we jailed
+    current_date=$(date) # overriding datetime
+    echo $current_date 'UNJAILING' 
+    echo -e "$WALLET_PASS\n" | umeed tx slashing unjail --chain-id=$NETWORK --from=$wallet --gas=auto --yes
+
+fi
+sleep 15 # repeating every 15 sec
+done
